e1dfda66eda5af5472c6f1200a4456371970193c
Oleh Hryb <oleg.grib.ua@gmail.com>
2023-01-16 21:35:35 +0000

    perf: ⚡️ updated OptimizedEnumerable to avoid duplication

--- contracts/package/tokens/OptimizedEnumerable.sol
+++ contracts/package/tokens/OptimizedEnumerable.sol
@@ -3,109 +3,77 @@
 pragma solidity ^0.8.16;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "./OptimizedEnumerableBase.sol";
 
-contract OptimizedEnumerable is IERC721Enumerable, ERC721 {
-	using Counters for Counters.Counter;
+contract OptimizedEnumerable is OptimizedEnumerableBase, ERC721 {
+	function _beforeTokenTransfer(
+		address from,
+		address to,
+		uint256 tokenId,
+		uint256 batchSize
+	) internal virtual override(OptimizedEnumerableBase, ERC721) {
+		OptimizedEnumerableBase._beforeTokenTransfer(from, to, tokenId, batchSize);
+		ERC721._beforeTokenTransfer(from, to, tokenId, batchSize);
+	}
 
-	Counters.Counter private _tokenIdCounter;
+	function balanceOf(
+		address owner
+	)
+		public
+		view
+		virtual
+		override(OptimizedEnumerableBase, ERC721)
+		returns (uint256 balance)
+	{
+		return ERC721.balanceOf(owner);
+	}
 
-	// count burnt token number to calc totalSupply()
-	uint256 private _burnt;
+	function ownerOf(
+		uint256 tokenId
+	)
+		public
+		view
+		virtual
+		override(OptimizedEnumerableBase, ERC721)
+		returns (address owner)
+	{
+		return ERC721.ownerOf(tokenId);
+	}
+
+	function _exists(
+		uint256 tokenId
+	)
+		internal
+		view
+		virtual
+		override(OptimizedEnumerableBase, ERC721)
+		returns (bool)
+	{
+		return ERC721._exists(tokenId);
+	}
+
+	function supportsInterface(
+		bytes4 interfaceId
+	)
+		public
+		view
+		virtual
+		override(ERC721, OptimizedEnumerableBase)
+		returns (bool)
+	{
+		return
+			ERC721.supportsInterface(interfaceId) ||
+			OptimizedEnumerableBase.supportsInterface(interfaceId);
+	}
 
 	constructor(
 		string memory name_,
 		string memory symbol_
 	) ERC721(name_, symbol_) {}
 
-	//slither-disable-next-line dead-code
-	function _beforeTokenTransfer(
-		address,
-		address to,
-		uint256,
-		uint256
-	) internal virtual override {
-		if (to == address(0)) {
-			_burnt++;
-		}
-	}
-
-	function supportsInterface(
-		bytes4 interfaceId
-	) public view virtual override(ERC721, IERC165) returns (bool) {
-		return
-			interfaceId == type(IERC721Enumerable).interfaceId ||
-			interfaceId == type(IERC165).interfaceId ||
-			super.supportsInterface(interfaceId);
-	}
-
-	/**
-	 * Foreach all minted tokens until reached appropriate index
-	 */
-	function tokenOfOwnerByIndex(
-		address owner,
-		uint256 index
-	) public view virtual override returns (uint256) {
-		require(index < balanceOf(owner), "Owner index out of bounds");
-
-		uint256 numMinted = _tokenIdCounter.current();
-		uint256 tokenIdsIdx = 0;
-
-		// Counter overflow is impossible as the loop breaks when uint256 i is equal to another uint256 numMintedSoFar.
-		unchecked {
-			for (uint256 i = 0; i < numMinted; i++) {
-				if (_exists(i) && (ownerOf(i) == owner)) {
-					if (tokenIdsIdx == index) {
-						return i;
-					}
-					tokenIdsIdx = tokenIdsIdx + 1;
-				}
-			}
-		}
-
-		// Execution should never reach this point.
-		assert(false);
-		// added to stop compiler warnings
-		return 0;
-	}
-
-	function totalSupply() public view virtual override returns (uint256) {
-		return _tokenIdCounter.current() - _burnt;
-	}
-
-	/**
-	 * @dev See {IERC721Enumerable-tokenByIndex}.
-	 */
-	function tokenByIndex(
-		uint256 index
-	) public view virtual override returns (uint256) {
-		uint256 numMintedSoFar = _tokenIdCounter.current();
-
-		require(index < totalSupply(), "Index out of bounds");
-
-		uint256 tokenIdsIdx = 0;
-
-		// Counter overflow is impossible as the loop breaks when uint256 i is equal to another uint256 numMintedSoFar.
-		unchecked {
-			for (uint256 i = 0; i < numMintedSoFar; i++) {
-				if (_exists(i)) {
-					if (tokenIdsIdx == index) {
-						return i;
-					}
-					tokenIdsIdx++;
-				}
-			}
-		}
-
-		// Execution should never reach this point.
-		assert(false);
-		return 0;
-	}
-
+    //slither-disable-next-line dead-code
 	function _mint(address to) internal {
-		uint newTokenId = _tokenIdCounter.current();
-		_tokenIdCounter.increment();
+		uint newTokenId = _prepareTokenId();
 		ERC721._mint(to, newTokenId);
 	}
 }
--- contracts/package/tokens/OptimizedEnumerableUpgradeable.sol
+++ contracts/package/tokens/OptimizedEnumerableUpgradeable.sol
@@ -3,107 +3,77 @@
 pragma solidity ^0.8.16;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/IERC721EnumerableUpgradeable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "./OptimizedEnumerableBase.sol";
 
-contract OptimizedEnumerableUpgradeable is
-	IERC721EnumerableUpgradeable,
+contract OptimizedEnumerableUpgradeable is 
+	OptimizedEnumerableBase,
 	ERC721Upgradeable
 {
-	using Counters for Counters.Counter;
 
-	Counters.Counter private _tokenIdCounter;
-
-	// count burnt token number to calc totalSupply()
-	uint256 private _burnt;
-
-	//slither-disable-next-line dead-code
-	function _beforeTokenTransfer(
-		address,
+    function _beforeTokenTransfer(
+		address from,
 		address to,
-		uint256,
-		uint256
-	) internal virtual override {
-		if (to == address(0)) {
-			_burnt++;
-		}
+		uint256 tokenId,
+		uint256 batchSize
+	) internal virtual override(OptimizedEnumerableBase, ERC721Upgradeable) {
+		OptimizedEnumerableBase._beforeTokenTransfer(from, to, tokenId, batchSize);
+		ERC721Upgradeable._beforeTokenTransfer(from, to, tokenId, batchSize);
 	}
 
-	function supportsInterface(
+	function balanceOf(
+		address owner
+	)
+		public
+		view
+		virtual
+		override(OptimizedEnumerableBase, ERC721Upgradeable)
+		returns (uint256 balance)
+	{
+		return ERC721Upgradeable.balanceOf(owner);
+	}
+
+	function ownerOf(
+		uint256 tokenId
+	)
+		public
+		view
+		virtual
+		override(OptimizedEnumerableBase, ERC721Upgradeable)
+		returns (address owner)
+	{
+		return ERC721Upgradeable.ownerOf(tokenId);
+	}
+
+	function _exists(
+		uint256 tokenId
+	)
+		internal
+		view
+		virtual
+		override(OptimizedEnumerableBase, ERC721Upgradeable)
+		returns (bool)
+	{
+		return ERC721Upgradeable._exists(tokenId);
+	}
+
+    function supportsInterface(
 		bytes4 interfaceId
 	)
 		public
 		view
 		virtual
-		override(ERC721Upgradeable, IERC165Upgradeable)
+		override(ERC721Upgradeable, OptimizedEnumerableBase)
 		returns (bool)
 	{
 		return
-			interfaceId == type(IERC721EnumerableUpgradeable).interfaceId ||
-			interfaceId == type(IERC165Upgradeable).interfaceId ||
-			super.supportsInterface(interfaceId);
+			ERC721Upgradeable.supportsInterface(interfaceId) ||
+			OptimizedEnumerableBase.supportsInterface(interfaceId);
 	}
 
-	/**
-	 * Foreach all minted tokens until reached appropriate index
-	 */
-	function tokenOfOwnerByIndex(
-		address owner,
-		uint256 index
-	) public view virtual override returns (uint256) {
-		require(index < balanceOf(owner), "Owner index out of bounds");
-
-		uint256 numMinted = _tokenIdCounter.current();
-		uint256 tokenIdsIdx = 0;
-
-		// Counter overflow is impossible as the loop breaks when uint256 i is equal to another uint256 numMintedSoFar.
-		unchecked {
-			for (uint256 i = 0; i < numMinted; i++) {
-				if (_exists(i) && (ownerOf(i) == owner)) {
-					if (tokenIdsIdx == index) {
-						return i;
-					}
-					tokenIdsIdx = tokenIdsIdx + 1;
-				}
-			}
-		}
-
-		// Execution should never reach this point.
-		assert(false);
-		// added to stop compiler warnings
-		return 0;
+    //slither-disable-next-line dead-code
+    function _mint(address to) internal {
+		uint newTokenId = _prepareTokenId();
+		ERC721Upgradeable._mint(to, newTokenId);
 	}
 
-	function totalSupply() public view virtual override returns (uint256) {
-		return _tokenIdCounter.current() - _burnt;
-	}
-
-	/**
-	 * @dev See {IERC721Enumerable-tokenByIndex}.
-	 */
-	function tokenByIndex(
-		uint256 index
-	) public view virtual override returns (uint256) {
-		uint256 numMintedSoFar = _tokenIdCounter.current();
-
-		require(index < totalSupply(), "Index out of bounds");
-
-		uint256 tokenIdsIdx = 0;
-
-		// Counter overflow is impossible as the loop breaks when uint256 i is equal to another uint256 numMintedSoFar.
-		unchecked {
-			for (uint256 i = 0; i < numMintedSoFar; i++) {
-				if (_exists(i)) {
-					if (tokenIdsIdx == index) {
-						return i;
-					}
-					tokenIdsIdx++;
-				}
-			}
-		}
-
-		// Execution should never reach this point.
-		assert(false);
-		return 0;
-	}
 }
